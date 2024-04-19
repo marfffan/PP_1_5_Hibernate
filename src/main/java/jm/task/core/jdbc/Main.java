fc2f5b39c06eec4efc1c2b4eba287cacaabbb3d6
marfffan <marfffan@gmail.com>
2024-04-19 11:53:24 +0300

    Добавил логирование в код, ошибка теперь выводится в консоль , ресурсы обернуты в try с ресурсами кроме connections он закрывается при удалении таблицы пользователей, ибо я что то не догадался как реализовать по другому.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
@@ -7,20 +8,29 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
 
+        UserDaoJDBCImpl udj = new UserDaoJDBCImpl();
+
         UserServiceImpl userService = new UserServiceImpl();
 
         userService.createUsersTable();
+        udj.createUsersTable();
 
         userService.saveUser("Ada", "Wong", (byte) 30);
+        udj.saveUser("Ada", "Wong", (byte) 30);
         userService.saveUser("Leon", "Walker", (byte) 32);
+        udj.saveUser("Leon", "Walker", (byte) 32);
         userService.saveUser("Dima", "Kuplinow", (byte) 35);
+        udj.saveUser("Dima", "Kuplinow", (byte) 35);
         userService.saveUser("Ashli", "Abama", (byte) 19);
 
         userService.getAllUsers();
+        udj.getAllUsers();
 
         userService.cleanUsersTable();
+        udj.cleanUsersTable();
 
         userService.dropUsersTable();
+        udj.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -6,8 +6,11 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl extends Util implements UserDao {
+    private static final Logger logger = Logger.getLogger(UserDaoJDBCImpl.class.getName());
 
     Connection conn = getConnection();
 
@@ -22,58 +25,75 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
                 " name VARCHAR(255)," +
                 " lastName VARCHAR(255)," +
                 " age TINYINT)";
-
-        try {
-            Statement statement = conn.createStatement();
+        // Оборачиваем Ststement в try с ресурсами для автоматического закрытия
+        try (Statement statement = conn.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка создания таблицы пользователей", e);
+            throw new RuntimeException("Ошибка создания таблицы пользователей", e);
         }
     }
 
     public void dropUsersTable() {
 
         String sql = "DROP TABLE IF EXISTS users";
-
+        // Оборачиваем Ststement в try с ресурсами для автоматического закрытия
         try (Statement statement = conn.createStatement()) {
             statement.executeUpdate(sql);
+            //Добавил закрытие ресурса connections ри удалении таблицы
+            conn.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка удаления таблицы пользователей", e);
+            throw new RuntimeException("Ошибка удаления таблицы пользователей", e);
         }
+        //Добавил закрытие ресурса connections ри удалении таблицы
+//        try (conn.close()) {
+//            System.out.println("Соединение закрыто");
+//        } catch (SQLException e) {
+//            //Добавляем логер и вывод ошибки в консоль
+//            logger.log(Level.SEVERE, "Ошибка закрытия соединения", e);
+//            throw new RuntimeException("Ошибка закрытия соединения", e);
+
     }
+
 
     public void saveUser(String name, String lastName, byte age) {
 
         String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
-
+        // Оборачиваем Ststement в try с ресурсами для автоматического закрытия
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка сохранения пользователя", e);
+            throw new RuntimeException("Ошибка сохранения пользователя", e);
         }
     }
 
     public void removeUserById(long id) {
 
         String sql = "DELETE FROM users WHERE id = ?";
-
+        // Оборачиваем Ststement в try с ресурсами для автоматического закрытия
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
             statement.setLong(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка удаления пользователя по ID", e);
+            throw new RuntimeException("Ошибка удаления пользователя по ID", e);
         }
     }
 
+
     public List<User> getAllUsers() {
-
         String sql = "SELECT * FROM users";
-
         List<User> userList = new ArrayList<>();
-
+        // Оборачиваем Ststement и resultset в try с ресурсами для автоматического закрытия
         try (PreparedStatement statement = conn.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
@@ -84,23 +104,26 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
                 user.setAge(resultSet.getByte("age"));
                 userList.add(user);
             }
-            for (User user : userList) {
-                System.out.println("User: " + user.getId() + " " + user.getName() + " " + user.getLastName() + " " + user.getAge());
-            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка возврата пользователей", e);
+            throw new RuntimeException("Ошибка возврата пользователей", e);
+
         }
         return userList;
     }
 
+
     public void cleanUsersTable() {
 
         String sql = "DELETE FROM users";
-
+        // Оборачиваем Ststement в try с ресурсами для автоматического закрытия
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
             statement.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            //Добавляем логер и вывод ошибки в консоль
+            logger.log(Level.SEVERE, "Ошибка очистки таблицы", e);
+            throw new RuntimeException("Ошибка очистки таблицы", e);
         }
     }
 }
